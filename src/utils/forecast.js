f448501a4fdf32de5c7d65a22f3a45d5d0269d37
Nikhil <nkk544@gmail.com>
2019-06-01 16:31:18 +0530

    Forcast Update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,11 +6,11 @@ const forecast = (latitude, longitude, callback) => {
         if (error) {
             callback('Unable to process request', undefined)
         } else if (body.error) {
-            callback('Unable to loacte', undefined)
+            callback('Unable to search the location', undefined)
         } else {
             const temp = body.currently.temperature;
             const precipProb = body.currently.precipProbability;
-            callback(undefined, ` ${body.daily.data[0].summary} It is currently ${temp} degrees out, There is a ${precipProb}% chance of rain`);
+            callback(undefined, ` ${body.daily.data[0].summary} It is currently ${temp} degrees out. Highest temprature of today is ${body.daily.data[0].temperatureHigh} degrees and Lowest temprature of today is ${body.daily.data[0].temperatureLow} degrees. There is a ${precipProb}% chance of rain.`);
         }
     })
 }
